d7643b9928603a9b763a16eb2c204bcb38f16d75
Murad <yusifovmurad1@gmail.com>
2024-12-09 13:55:00 +0300

    Fixed deploy issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // @ts-ignore
     <ClerkProvider>
       <html lang="en">
         <body className={`${quicksand.className} antialiased`}>
