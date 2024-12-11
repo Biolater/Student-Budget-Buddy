c690da8ead33527bea55a38912d667ac3f6f22cc
Murad <yusifovmurad1@gmail.com>
2024-12-11 22:31:17 +0300

    Display default values for dashboard

--- src/app/(home)/dashboard/BudgetData.tsx
+++ src/app/(home)/dashboard/BudgetData.tsx
@@ -4,13 +4,32 @@ import {
   CardBody,
   CardFooter,
   Divider,
-  Link,
+  Link as nextUILink,
   Image,
   Progress,
 } from "@nextui-org/react";
-import { BarChart3, DollarSign, TrendingUp, Wallet } from "lucide-react";
+import {
+  ArrowRight,
+  BarChart3,
+  DollarSign,
+  TrendingUp,
+  Wallet,
+} from "lucide-react";
+import Link from "next/link";
 
-const BudgetData = () => {
+type BudgetDataProps = {
+  totalBudget?: number | null;
+  spent?: number | null;
+  savingsGoal?: number | null;
+  savedAmount?: number | null;
+};
+
+const BudgetData: React.FC<BudgetDataProps> = ({
+  totalBudget,
+  spent,
+  savingsGoal,
+  savedAmount,
+}) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
       <Card>
@@ -18,9 +37,24 @@ const BudgetData = () => {
           <h3 className="tracking-tight text-sm font-medium">Total Budget</h3>
           <DollarSign className="size-4 text-muted-foreground" />
         </CardHeader>
-        <CardBody className="p-6 pt-0">
-          <div className="text-2xl font-bold">$2000.00</div>
-          <p className="text-xs text-muted-foreground">For this semester</p>
+        <CardBody className="p-6 pt-0 space-y-2">
+          {totalBudget ? (
+            <>
+              <div className="text-2xl font-bold">$2000.00</div>
+              <p className="text-xs text-muted-foreground">For this semester</p>
+            </>
+          ) : (
+            <>
+              <p className="text-sm text-muted-foreground">No budget set</p>
+              <Link
+                href="/budget"
+                className="inline-flex self-start items-center gap-2 whitespace-nowrap rounded-md text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 underline-offset-4 hover:underline p-0 h-auto font-normal"
+              >
+                Add Budget
+                <ArrowRight className="size-4 ml-1" />
+              </Link>
+            </>
+          )}
         </CardBody>
       </Card>
       <Card>
@@ -28,9 +62,32 @@ const BudgetData = () => {
           <h3 className="tracking-tight text-sm font-medium">Spent</h3>
           <Wallet className="size-4 text-muted-foreground" />
         </CardHeader>
-        <CardBody className="p-6 pt-0">
-          <div className="text-2xl font-bold">$1080.00</div>
-          <p className="text-xs text-muted-foreground">54.0% of total budget</p>
+        <CardBody className="p-6 pt-0 space-y-2">
+          {spent ? (
+            <>
+              {" "}
+              <div className="text-2xl font-bold">$1080.00</div>
+              <p className="text-xs text-muted-foreground">
+                54.0% of total budget
+              </p>
+            </>
+          ) : (
+            <>
+              {" "}
+              <>
+                <p className="text-sm text-muted-foreground">
+                  No No expenses recorded
+                </p>
+                <Link
+                  href="/budget"
+                  className="inline-flex self-start items-center gap-2 whitespace-nowrap rounded-md text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 underline-offset-4 hover:underline p-0 h-auto font-normal"
+                >
+                  Add expenses
+                  <ArrowRight className="size-4 ml-1" />
+                </Link>
+              </>
+            </>
+          )}
         </CardBody>
       </Card>
       <Card>
